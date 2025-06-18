b6dc5491254694ec3d9e83a5863d770bf1980266
MUBASHIRKHAN-PROGRAMMER <mk765pc@gmail.com>
2025-06-18 12:03:11 +0500

    Age calculator

--- script.js
+++ script.js
@@ -1,43 +1,35 @@
-function AgeSubmit(){
+function AgeSubmit() {
     let currentyear = document.getElementById("Agecurrent").value;
-    let cuyear = new Date(currentyear);
-    let diffCurrent = cuyear.getFullYear();
+    let curyear = new Date(currentyear);
+    let diffCurrent = curyear.getFullYear();
 
     let yearUs = document.getElementById("Ageuser").value;
     let useryear = new Date(yearUs);
     let userConvert = useryear.getFullYear();
 
-    let diffyearResult = diffCurrent -  userConvert ;
+    let diffyearResult = diffCurrent - userConvert;
     ///month///
     let currentMonth = document.getElementById("Agecurrent").value;
-    let moDate = new Date(currentMonth);
-    let diffMonth = moDate.getMonth();
+    let monDate = new Date(currentMonth);
+    let diffMonth = monDate.getMonth() + 1;
 
     let monthUs = document.getElementById("Ageuser").value;
     let monthDate = new Date(monthUs);
-    let monthConvert = monthDate.getMonth();
+    let monthConvert = monthDate.getMonth() + 1;
 
-    let diffMonResult = diffMonth -  monthConvert ;
-   //days//
-   let currDay = document.getElementById("Agecurrent").value;
-   let currentDay = new Date(currDay);
-   let diffDays = currentDay.getTime();
+    let diffMonResult = Math.abs(diffMonth - monthConvert);
+    //days//
+    let currDate = document.getElementById("Agecurrent").value;
+    let currentDate = new Date(currDate);
+    let diffDates = currentDate.getDate();
 
-   let userDay = document.getElementById("Agecurrent").value;
-   let useDay = new Date(userDay);
-   let difUsDays = useDay.getTime();
-  
-        let diffDay = diffDays - difUsDays;
+    let userDates = document.getElementById("Ageuser").value;
+    let useDate = new Date(userDates);
+    let difUsDate = useDate.getDate();
+    let diffDate = Math.abs(diffDates - difUsDate);
 
-        let second = diffDay / 1000;
-        let minute = second / 60;
-        let hour = minute / 60;
-        let day = hour / 24;
-
-
-    document.getElementById("Result").innerText = `Age: ${diffyearResult} `;
-
-    document.getElementById("ResultMo").innerText = `Month: ${diffMonResult} `;
-
-    document.getElementById("ResultDay").innerText = `Days: ${day} `;
+    document.getElementById("Result").innerText = `Age: ${diffyearResult} years`;
+    document.getElementById("ResultMo").innerText = `Month: ${diffMonResult} months`;
+    document.getElementById("ResultDay").innerText = `Days: ${diffDate} days`;
 }
+// https://themewagon.github.io/gymlife/
